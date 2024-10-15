19a996fdac02222544f1f0d0a66baf4ec151532a
coca-create <himotoki.sansu@gmail.com>
2024-10-15 19:00:01 +0900

    Update tab1_func.py

--- tab1/tab1_func.py
+++ tab1/tab1_func.py
@@ -104,6 +104,7 @@ def create_excel_from_srt_c(srt_content, input_file_name):
     return df_display'''
 
 def transcribe(File, Model, Computing, Lang, BeamSize, VadFilter, device, progress=gr.Progress()):
+    print(File)
     save_folder = "/content/drive/My Drive/whisper_uploads"
     File=os.path.join(save_folder,File)
     if not File:
