01f46d980c5c3c06059116bc533be14d9462f887
coca-create <himotoki.sansu@gmail.com>
2024-10-15 16:31:48 +0900

    Update tab1_func.py

--- tab1/tab1_func.py
+++ tab1/tab1_func.py
@@ -104,6 +104,7 @@ def create_excel_from_srt_c(srt_content, input_file_name):
     return df_display'''
 
 def transcribe(File, Model, Computing, Lang, BeamSize, VadFilter, device, progress=gr.Progress()):
+    File=os.path.join(save_folder,File)
     if not File:
         print("ファイルが提供されていません")
         return"", "", "", [], [], "", "", "", "", "",""
